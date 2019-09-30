f2a60d8e00e0892ce9c9698c8d103bc398da3bd8
Zana Daniel <zanadaniel@pm.me>
2019-09-30 09:12:19 +0100

    v0.1.1
    - Added Material.
    - Added MDC Layout Grid.
    - Added FontAwesome just in case.
    - Set up routing options.
    - Set up custom theme using the teal color.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+    MatIconModule,
+    MatButtonModule
+} from "@angular/material";
 
 @NgModule({
     declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
